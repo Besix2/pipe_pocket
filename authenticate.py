601e803212b592ff19b66d3b61de81279cd860c6
Besix2 <myusernamethis@outlook.de>
2023-05-06 11:59:53 +0200

    fixed authenticator

---
+++ authenticate.py
@@ -0,0 +1,15 @@
+from pocket import Pocket
+
+# Set up Pocket API credentials
+consumer_key = "your-consumer-key"
+redirect_uri = "https://getpocket.com/de/home"
+pocket_instance = Pocket(consumer_key, access_token=None)
+
+# Obtain a request token
+request_token = pocket_instance.get_request_token(redirect_uri=redirect_uri, consumer_key=consumer_key)
+auth_url = Pocket.get_auth_url(code=request_token, redirect_uri=redirect_uri)
+print(auth_url)
+input("Finished?(press enter)")
+user_credentials = Pocket.get_credentials(consumer_key=consumer_key, code=request_token)
+access_token = user_credentials['access_token']
+print(f"Your acces token: {access_token}")
